e67c3de82714b10b545c832285b9ee46d7019958
bamboo <1972605058@qq.com>
2020-11-14 16:26:35 +0800

    添加了servlet代码

---
+++ src/main/java/bamboo/Servlet.java
@@ -0,0 +1,26 @@
+package bamboo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author Bamboo
+ * @Date 2020/11/14 16:25
+ * @Version 1.0
+ */
+@WebServlet(name = "Servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
